2f84cb6c1675d6fb0124a94b3426e1d279381188
dwngrangto-glitch <dwngrangto@gmail.com>
2026-01-09 09:56:43 +0700

    Update comments for clarity in Ngay1.c

--- Ngay1.c
+++ Ngay1.c
@@ -2,7 +2,7 @@
 int main(){
 	int tuoi = 20;
 	char hoten[] = "Nguyen Hoang Hai Dang";
-	printf("Ten t la: %s\n", hoten);    //%s là khai báo kiểu kí tự//
-	printf("Tuoi t la: %d", tuoi);     //%d là khai báo kiểu số nguyên//
+	printf("Ten t la: %s\n", hoten);    //%s là khai báo kiểu kí tự dạng chuỗi, %c là khai báo char
+	printf("Tuoi t la: %d", tuoi);     //%d là khai báo kiểu số nguyên
 	return 0;
 }

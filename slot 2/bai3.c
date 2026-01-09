51635eac6d94fd0107ab803f788cd82a07242904
Nguyễn Hoàng Hải Đăng <dwngrangto@gmail.com>
2026-01-09 11:43:32 +0700

    Update bai3.c

--- slot 2/bai3.c
+++ slot 2/bai3.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-int main (){
-	float a = 5;
-	float b = 10.216;
-	printf("a= %.0f\n", a);
-	printf("b= %.3f\n", b);
-	printf("5+10.216=15.22");
-	return 0;
+int main() {
+    float a = 5;
+    float b = 10.216;
+    float c = a + b;
+    printf("a = %.0f\n", a);
+    printf("b = %.3f\n", b);
+    printf("%.0f + %.3f = %.2f", a, b, c);
+    return 0;
 }

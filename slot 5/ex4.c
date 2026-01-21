de0b4026e58b0afc7512587e328036e3bb961390
Your Name <you@example.com>
2026-01-21 09:39:18 +0700

    slot5\ex5

--- slot 5/ex4.c
+++ slot 5/ex4.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-int main(){
-	int soTien;
-	int soKwH;
-	printf("Nhap so KwH: ");
-	scanf("%d", &soKwH);
-	if (soKwH < 100) {
-		soTien = soKwH * 950;
-	} else 
-		if (soKwH <= 150 && soKwH > 100){
-		soTien = soKwH * 1250;
-	} else 
-		if (soKwH <= 200 && soKwH > 150){
-		soTien = soKwH * 1350;
-	} else 
-		soTien = soKwH * 1550;
-	printf("So tien dien la: %d", soTien);
-	return 0;
+int main() {
+    int soTien;
+    int soKwH;
+    printf("Nhap so KwH: ");
+    scanf("%d", &soKwH);
+    if (soKwH <= 100) {
+        soTien = soKwH * 950;
+    } 
+    else if (soKwH <= 150) {
+        soTien = (100 * 950) + (soKwH - 100) * 1250;
+    } 
+    else if (soKwH <= 200) {
+        soTien = (100 * 950) + (50 * 1250) + (soKwH - 150) * 1350;
+    } 
+    else {
+        soTien = (100 * 950) + (50 * 1250) + (50 * 1350) + (soKwH - 200) * 1550;
+    }
+    printf("So tien dien la: %d VND\n", soTien);
+    return 0;
 }
